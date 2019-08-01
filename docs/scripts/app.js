afbd2edcac87b5ce727e80a16bb0fb475d209648
Ovardov <ovardov7@gmail.com>
2019-08-01 14:52:03 +0300

    Sticky header after scroll

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -1,16 +1,20 @@
-let goTopButton = document.querySelector('.go-top');
+let siteHeader = document.querySelector('.site-header');
 
+let goTopButton = document.querySelector('.go-top');
 goTopButton.addEventListener('click', goTop);
-window.addEventListener('scroll', showButton);
+
+window.addEventListener('scroll', showButtonAndStickyNav);
 
 function goTop() {
     document.body.window.pageYOffset = 0;
 }
 
-function showButton() {
+function showButtonAndStickyNav() {
     if (window.pageYOffset > 0) {
         goTopButton.style.opacity = 1;
+        siteHeader.classList.add('fixed');
     } else {
         goTopButton.style.opacity = 0;
+        siteHeader.classList.remove('fixed');
     }
 }
