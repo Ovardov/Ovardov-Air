125b4345faa1ca34dfe0a6a547dffd0660a73602
Ovardov <ovardov7@gmail.com>
2019-08-06 16:09:35 +0300

    Add content in home page

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -5,6 +5,10 @@ goTopButton.addEventListener('click', goTop);
 
 window.addEventListener('scroll', showButtonAndStickyNav);
 
+let allQuestionsElements = document.querySelectorAll('.questions-wrapper');
+
+allQuestionsElements.forEach(questionWrapper => questionWrapper.addEventListener('click', showOrHideAnswer));
+
 function goTop() {
     document.body.pageYOffset = 0;
 }
@@ -16,5 +20,15 @@ function showButtonAndStickyNav() {
     } else {
         goTopButton.style.opacity = 0;
         siteHeader.classList.remove('fixed');
+    }
+}
+
+function showOrHideAnswer() {
+    let questionWrapper = event.target;
+
+    if(questionWrapper.classList.contains('active')) {
+        questionWrapper.classList.remove('active');
+    } else {
+        questionWrapper.classList.add('active');
     }
 }
